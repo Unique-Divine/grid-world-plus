07e8cb2f90a08ef3fde19e46022c5dcb43b09902
Unique Divine <u.divine@columbia.edu>
2021-03-07 15:05:44 -0500

    Fixed silent errors in discount rate and learning rate methods. Added tests for them too

--- tools.py
+++ tools.py
@@ -1,12 +1,28 @@
 import numpy as np
 
-def discount_rate(ep_num, g=.95):
-    return g
+def discount_rate(episode_idx, num_episodes,  discount=.95):
+    """[summary]
+
+    Args:
+        episode_idx (int): The integer encoding for the episode.
+        num_episodes (int): The total number of episodes.
+        discount (float, optional): Discount factor. Defaults to .95.
+            https://tinyurl.com/discount-stack-exchange
+
+    Returns:
+        [type]: [description]
+    """
+    assert discount <= 1, "Discount must be between 0 and 1"
+    assert discount >= 0, "Discount must be between 0 and 1"
+    return discount
 
 
-def lr(ep_num, num_eps):
+def lr(episode_idx, num_episodes):
     # 1, .99, .98, .97 ,...., 0
-    return 1 - ep_num/num_eps
+    lr = 1 - episode_idx/num_episodes
+    assert lr <= 1, "'lr' must be between 0 and 1"
+    assert lr >= 0, "'lr' must be between 0 and 1"
+    return lr
 
 
 # returns an integer in 0,...,NUM_ACTIONS
