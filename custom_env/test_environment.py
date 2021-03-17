c16fea266eb6a3f6098c60c2e6fa062695da13c2
Unique Divine <u.divine@columbia.edu>
2021-03-17 18:10:24 -0400

    fix & test: Updated tests to account for recent refactors. All bugs have been fixed.

--- custom_env/test_environment.py
+++ custom_env/test_environment.py
@@ -4,8 +4,13 @@ import numpy as np
 
 def test_set_agent_goal():
     env = environment.Environment(grid_shape=(50, 50), n_goals=60)
-    env.set_agent_goal()
+    for item_name in ['agent', 'hole', 'goal', 'blocked']:
+        n_items = int((env.grid == env.interactables[item_name]).sum())
+        expected_count = 0
+        assert n_items == expected_count, \
+            f"Too many '{item_name}' items on the board."
 
+    env.set_agent_goal()
     # Find interactables such as the agent and goal
     nonfrozen_spots: np.ndarray = np.argwhere(
         env.grid != env.interactables['frozen'])
@@ -30,31 +35,37 @@ def test_set_agent_goal():
         assert env_object in list(nonfrozen.values())
     
     # TODO: Make sure n_goals is actually 60.
-
-    print(env.grid)
-    # print(env.open_positions)
     
 
 def test_set_holes():
     env = environment.Environment(grid_shape=(50, 50), n_goals=20)
+
     env.set_agent_goal()
-    env.set_holes()
-
-    n_agents: int = int((env.grid == env.interactables['agent']).sum())
-    n_goals: int = int((env.grid == env.interactables['goal']).sum())
     n_holes: int = int((env.grid == env.interactables['hole']).sum())
-    n_frozen: int = int((env.grid == env.interactables['frozen']).sum())
-    n_positions = np.product(env.grid.shape)
+    assert n_holes == 0, "Nonzero number of holes before creation." 
+
+    # Calculate expected number of holes before they are placed on the board.
+    n_agents = int((env.grid == env.interactables['agent']).sum())
+    n_goals = int((env.grid == env.interactables['goal']).sum())
+    n_frozen = int((env.grid == env.interactables['frozen']).sum())
+    n_positions = np.product(env.grid.shape) 
     assert n_positions == np.sum([n_agents, n_goals, n_holes, n_frozen])
-
-    n_previously_frozen = n_positions - n_agents - n_goals 
+    n_previously_frozen = n_frozen
     expected_n_holes = int(n_previously_frozen * env.hole_pct)
-    assert n_holes == expected_n_holes
-    
-    print(env.grid[:10,:10])
 
-test_set_agent_goal()
-print("Test passed.")
-test_set_holes()
-print("Test passed.")
+    # Place holes on the board
+    env.set_holes() 
+    n_holes: int = int((env.grid == env.interactables['hole']).sum())
+    assert n_holes != 0, "No holes were created by 'set_holes()'. Odd."
+    n_frozen = int((env.grid == env.interactables['frozen']).sum())
 
+    assert n_holes == expected_n_holes, \
+        "Mistakes were made in 'env.set_holes()'"
+
+def run_all_tests(verbose = True):
+    for test in [test_set_agent_goal, test_set_holes]:
+        test()
+        print(f"Test passed: '{test}'" if verbose else "")
+ 
+if __name__ == "__main__":
+    run_all_tests()
