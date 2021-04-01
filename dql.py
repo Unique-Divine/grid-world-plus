e11e28ee50fad8383843440d950812387bb93ce3
eskalnes <33837551+eskalnes@users.noreply.github.com>
2021-04-01 19:40:35 -0400

    progress

--- dql.py
+++ dql.py
@@ -1,17 +1,24 @@
 import torch
 import numpy as np
 import gym
+import random
 import sys
 import os
+import collections
+Experience = collections.namedtuple(
+    "Experience",
+    ["obs", "a", "r", "next_obs", "d"])
 
 from models.DQN.q_network import Q
 from models.DQN.tools import epsilon, run_target_update, plot_episode_rewards, ReplayBuffer
-from custom_env import environment
 
-breakpoint()
+from custom_env.agent import Agent
+from custom_env.environment import Env, State, PathMaker
+
+
 
 # buffer hyperparameters
-batchsize = 200  # batchsize for buffer sampling
+batchsize = 2  # batchsize for buffer sampling
 buffer_maxlength = 1000  # max number of tuples held by buffer
 episodes_til_buffer_sample = 2
 buffer = ReplayBuffer(buffer_maxlength)  # buffer holds the memories of the exp replay
@@ -25,67 +32,68 @@ gamma = .99  # discount
 list_of_episode_rewards = []  # records the reward per episode
 Qtarget_update_counter = 0  # count the number of steps taken before updating Qtarget
 
-# initialize environment
-envname = "CartPole-v0"
-env = gym.make(envname)
-"""
-obssize
-Num	Observation	           Min	    Max
-0	Cart Position	      -2.4	    2.4
-1	Cart Velocity	      -Inf	    Inf
-2	Pole Angle	          -41.8°	41.8°
-3	Pole Velocity At Tip  -Inf	    Inf
-"""
+# initialize environment and agent
+env = Env(grid_shape=(5, 5), n_goals=3, hole_pct=0.1)
+james_bond = Agent(4)
+env.create()
 
 # initialize the principal and the target Q nets
-state_dim = env.observation_space.low.size
-action_dim = env.action_space.n
+state = State(env, james_bond)
+state_dim = state.observation.size
+action_dim = len(env.action_space)
 lr = 1e-3
 QP = Q(state_dim, action_dim, lr)
 QT = Q(state_dim, action_dim, lr)
 
 for episode in range(num_episodes):
-    s = env.reset()
+    env.reset()
     d = False
     reward_sum = 0
 
+    scene_number = 0
     while not d:
-        q_vals = QP.predict_state_value(s)
+        state = State(env, james_bond)
+        q_vals = QP.predict_state_value(state.observation.flatten())
 
         # Execute actions
         if np.random.rand() < epsilon(episode, num_episodes):
-            a = torch.tensor(env.action_space.sample())
+            a = np.random.randint(0, action_dim)
         else:
             a = torch.argmax(q_vals)
-        assert a in [0, 1]
+        a = int(a)
+        assert a in np.arange(0, action_dim).tolist()
 
-        ns, r, d, _ = env.step(int(a))
-        d_ = 1 if d else 0
-        e = (s, a, r, d_, ns)  # memory tuple
+        """ better to not save ns, but implement the alg to look ahead """
+        ns, r, d, info = env.step(action_idx=a, state=state)  # ns is the new state observation bc of vision window
+        e = Experience(state.observation, a, r, ns, d)
+        # e = (state.observation, a, r, ns, d)  # memory tuple
         buffer.append(e)  # buffer is the experience replay
         while buffer.number > buffer_maxlength:
             buffer.pop()
 
+        if scene_number > 25:
+            break
+
         # Training theta by gradients
 
-        if episode % episodes_til_buffer_sample == 0 and buffer.number > batchsize:
-            batch = buffer.sample(batchsize)  # list of memory tuples (current_state, action, r, done_, new_state)
-            batch = list(map(list, zip(*batch)))
-            states = batch[0]
-            actions = batch[1]
-            rewards = batch[2]
-            dones = batch[3]
-            next_states = batch[4]
+    if episode % episodes_til_buffer_sample == 0 and buffer.number > batchsize:
+        batch = buffer.sample(batchsize)  # list of memory tuples (current_state, action, r, done_, new_state)
+        batch = list(map(list, zip(*batch)))
+        states = [s.flatten() for s in batch[0]]
+        actions = batch[1]
+        rewards = batch[2]
+        next_states = [ns.flatten() for ns in batch[3]]
+        dones = batch[4]
 
-            q_vals_ns = QT.predict_state_value(next_states)
-            max_vals = torch.max(q_vals_ns, dim=1).values  # take the max along the columns
-            targets = torch.tensor(rewards) + torch.tensor(gamma)*max_vals
+        q_vals_ns = QT.predict_state_value(next_states)
+        max_vals = torch.max(q_vals_ns, dim=1).values  # take the max along the columns
+        targets = torch.tensor(rewards) + torch.tensor(gamma)*max_vals
 
-            done_indices = [i for i, d in enumerate(dones) if d==1]
-            for idx in done_indices:
-                targets[idx] = rewards[idx]
+        done_indices = [i for i, d in enumerate(dones) if d==1]
+        for idx in done_indices:
+            targets[idx] = rewards[idx]
 
-            QP.train(states, actions, targets)
+        QP.train(states, actions, targets)
 
         # 5)
         if Qtarget_update_counter % steps_til_target_update == 0:
@@ -94,7 +102,8 @@ for episode in range(num_episodes):
         # 6)
         Qtarget_update_counter += 1
         reward_sum += r
-        s = ns
+        print(f"episode {episode}")
+
 
     list_of_episode_rewards.append(reward_sum)
 
