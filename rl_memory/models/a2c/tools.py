47064e432739d9b026ef6e913183d6c3afbe209a
Unique Divine <u.divine@columbia.edu>
2021-08-09 14:28:05 -0500

    docs (tools.py): Add documentation for 'epsilon' method and exceptions for invalid arguments

--- rl_memory/models/a2c/tools.py
+++ rl_memory/models/a2c/tools.py
@@ -1,18 +1,4 @@
 import numpy as np
-from IPython.display import clear_output
-import matplotlib.pyplot as plt
-
-
-def epsilon(current_episode, num_episodes):
-    """
-    epsilon decays as the current episode gets higher because we want the agent to
-    explore more in earlier episodes (when it hasn't learned anything)
-    explore less in later episodes (when it has learned something)
-    i.e. assume that episode number is directly related to learning
-    """
-    # return 1 - (current_episode/num_episodes)
-    return .5 * .9**current_episode
-
 
 def discounted_reward(r, gamma):
     """ take 1D float array of rewards and compute discounted reward """
@@ -24,39 +10,3 @@ def discounted_reward(r, gamma):
     return list(discounted_r)
 
 
-def plot_episode_rewards(values, title, reset_frequency):
-    """ Plot the reward curve and histogram of results over time."""
-    # Update the window after each episode
-    clear_output(wait=True)
-    x = range(len(values))
-
-    # Define the figure
-    f, ax = plt.subplots(nrows=1, ncols=2, figsize=(12,5))
-    f.suptitle(title)
-    ax[0].plot(values, label='score per run')
-    ax[0].axhline(1, c='red', ls='--', label='goal')
-    ax[0].set_xlabel('Episodes')
-    ax[0].set_ylabel('Reward')
-    ax[0].legend()
-
-    # xcoords = [i for i in x if i%reset_frequency==0]
-    # for xc in xcoords:
-    #     plt.axvline(x=xc, color='k', linestyle='--')
-
-    # Calculate the trend
-    try:
-        z = np.polyfit(x, values, 1)
-        p = np.poly1d(z)
-        ax[0].plot(x, p(x), "--", label='trend')
-    except:
-        print('')
-
-
-
-    # Plot the histogram of results
-    ax[1].hist(values[-50:])
-    ax[1].axvline(1, c='red', label='goal')
-    ax[1].set_xlabel('Scores per Last 50 Episodes')
-    ax[1].set_ylabel('Frequency')
-    ax[1].legend()
-    plt.show()
