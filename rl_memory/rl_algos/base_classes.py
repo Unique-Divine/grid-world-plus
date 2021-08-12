54c143f0dfe18cc6c2cfd2deef9652655d94a2ee
Unique Divine <u.divine@columbia.edu>
2021-08-12 00:40:37 -0500

    dev (rl_algos/base.py): Implement ABC for RL Algorithms

--- rl_memory/rl_algos/base_classes.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-class RLAlgorithm(ABC):
-    """Representation of a reinforcement learning algorithm"""
-
-    @abstractmethod
-    def TODO:
-        """TODO"""
